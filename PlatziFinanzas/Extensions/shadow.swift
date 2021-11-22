3f95cf78f832632a28da39c3a7c6c775f8991421
Saul Corona <saulcoronascs@gmail.com>
2021-11-22 15:42:01 -0600

    Add custom colors, icons and key paths

---
+++ PlatziFinanzas/Extensions/shadow.swift
@@ -0,0 +1,22 @@
+//
+//  shadow.swift
+//  PlatziFinanzas
+//
+//  Created by saul corona on 22/11/21.
+//
+
+import UIKit
+
+extension UIView {
+    var borderUIColor: UIColor {
+        get {
+            guard let color = layer.borderColor else {
+                return UIColor.black
+            }
+            return UIColor(cgColor: color)
+        }
+        set {
+            layer.borderColor = newValue.cgColor
+        }
+    }
+}
